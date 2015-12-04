0a0e972b606e5d0cbb86e07a36bf6d80e3d7d30a
Serban Teodorescu <teodorescu.serban@gmail.com>
2015-12-04 22:24:13 +0200

    _install: added instructions for workon.sh :D

--- _install/workon.sh
+++ _install/workon.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# 2015 copyleft "Serban Teodorescu <teodorescu.serban@gmail.com>"
+
+# How to use it?
+# Simply type: . workon.sh
 
 source lib/_docker_aliases
 echo "Aliases have been [re]added. Enjoy!"
